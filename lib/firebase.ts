83c5e6cf646a00cdb25f23ad16122765b0b193a6
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2026-03-07 23:34:54 +0000

    feat: custom branding  for google sign in

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -12,7 +12,7 @@ import {
 // TODO: Replace with your own config
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "lap.onl",
+  authDomain: "auth.lap.onl",
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
