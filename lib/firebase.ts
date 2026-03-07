49a303c15a11400fcb7e4f746bd4e4e7bd449e55
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2026-03-07 23:17:06 +0000

    feat: Integrate Firebase client SDK for auth, storage, firestore, and functions, and add Netlify redirect for authentication.

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -12,7 +12,7 @@ import {
 // TODO: Replace with your own config
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  authDomain: "lap.onl",
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
@@ -39,7 +39,7 @@ if (typeof window !== "undefined") {
   enableMultiTabIndexedDbPersistence(db).catch((err) => {
     if (err.code === "failed-precondition") {
       console.warn(
-        "Firestore persistence has been disabled because multiple tabs are open"
+        "Firestore persistence has been disabled because multiple tabs are open",
       );
     } else if (err.code === "unimplemented") {
       console.warn("Browser does not support the required IndexedDB features");
