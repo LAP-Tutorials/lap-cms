ecf4125814af1103473ae354e6638cb53aeb417e
Llewellyn Adonteng Paintsil <Llewellynpaintsil34@gmail.com>
2026-03-08 05:37:25 +0000

    feat: infrastructure lockdown against search engine crawlers and automated bots is complete!

--- next.config.ts
+++ next.config.ts
@@ -1,44 +1,48 @@
 import type { NextConfig } from "next";
 
-
 const nextConfig: NextConfig = {
   /* config options here */
   async headers() {
     return [
       {
-        source: '/:path*',
+        source: "/:path*",
         headers: [
           {
-            key: 'X-DNS-Prefetch-Control',
-            value: 'on'
+            key: "X-DNS-Prefetch-Control",
+            value: "on",
           },
           {
-            key: 'Strict-Transport-Security',
-            value: 'max-age=63072000; includeSubDomains; preload'
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
           },
           {
-            key: 'X-XSS-Protection',
-            value: '1; mode=block'
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
           },
           {
-            key: 'X-Frame-Options',
-            value: 'DENY' // Preventing clickjacking
+            key: "X-Frame-Options",
+            value: "DENY", // Preventing clickjacking
           },
           {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff'
+            key: "X-Content-Type-Options",
+            value: "nosniff",
           },
           {
-            key: 'Referrer-Policy',
-            value: 'strict-origin-when-cross-origin'
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
           },
           {
-             key: 'Permissions-Policy',
-             value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()'
-          }
+            key: "Permissions-Policy",
+            value:
+              "camera=(), microphone=(), geolocation=(), interest-cohort=()",
+          },
+          {
+            key: "X-Robots-Tag",
+            value: "noindex, nofollow, nosnippet, noarchive, noimageindex",
+          },
         ],
       },
-    ]
+    ];
   },
 };
 
